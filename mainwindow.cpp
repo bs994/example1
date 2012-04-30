c7641dd200d61433b4ef83e0176ed36610be39d3
Mostafa Hasanabadi <bs994@yahoo.com>
2012-04-30 13:13:54 +0430

    example form

---
+++ mainwindow.cpp
@@ -0,0 +1,27 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QDialog>
+#include <QWidget>
+#include <QString>
+#include "myform.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    myForm = new myform;
+    ui->pushButton->setText("CLICK");
+
+    connect(ui->pushButton , SIGNAL(clicked()) , this , SLOT(newDialog2()));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+void MainWindow::newDialog2(){
+   MainWindow::myForm->str = "This str changed in main window class ";
+   QString str2 = MainWindow::myForm->str ;
+   MainWindow::myForm->label->setText(str2);
+}
