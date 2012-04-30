f5a32386efce56e3f8bb87d19af7e315db6c37e4
Mostafa Hasanabadi <bs994@yahoo.com>
2012-04-30 13:14:44 +0430

    example form

---
+++ myform.h
@@ -0,0 +1,15 @@
+#ifndef MYFORM_H
+#define MYFORM_H
+
+#include <QtGui>
+
+class myform
+{
+    public:
+        myform();
+        ~myform();
+        QString str ;
+        QLabel *label;
+};
+
+#endif // MYFORM_H
